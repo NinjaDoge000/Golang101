be79e4b5d2a1fbf1ad9acc03e9c95ff02f9b2d0a
NinjaDoge000 <Ninjadoge000@gmail.com>
2025-03-23 17:51:31 +0530

    embeded structs

--- Structs/struct.go
+++ Structs/struct.go
@@ -49,6 +49,27 @@ func NewUser(firstName, lastName, birthDate string) (*user, error) {
 	}, nil
 }
 
+// embeded struct
+type admin struct {
+	user
+	password string
+	email    string
+}
+
+// constructor function for admin using user constructor
+func NewAdmin(firstName, lastName, birthDate, password, email string) (*admin, error) {
+	u, err := NewUser(firstName, lastName, birthDate)
+	if err != nil {
+		return nil, err
+	}
+
+	return &admin{
+		user:     *u,
+		password: password,
+		email:    email,
+	}, nil
+}
+
 func main() {
 
 	var appUser user = user{
@@ -65,12 +86,33 @@ func main() {
 	DisplayUser(&appUser)
 	appUser.getUser()
 	appUser.clearUser()
+	// after clearing the user
 	appUser.getUser()
 
-	myAppUser, err := NewUser("", "", "01/01/1991")
+	// myAppUser, err := NewUser("", "", "01/01/1991")
+	// if err != nil {
+	// 	println(err.Error())
+	// 	return
+	// }
+
+	// DisplayUser(myAppUser) // empty user
+
+	// creating admin user
+	adminUser, err := NewAdmin("Jane", "Doe", "01/01/1991", "password", "jon@email.com")
 	if err != nil {
 		println(err.Error())
 		return
 	}
-	DisplayUser(myAppUser)
+
+	println("Admin User")
+	DisplayUser(&adminUser.user)
+	println("Password: ", adminUser.password)
+
+	/*
+
+		Note: while Exporting struct fields, the first letter of the field name should be in uppercase.
+		and while Exporting struct methods, the first letter of the method name should be in uppercase.
+		and while Exporting struct constructor functions, the first letter of the function name should be in uppercase.
+
+	*/
 }
